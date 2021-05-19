c2940bf78eed9130e0b36a0924bc619b71151fd9
robertadriang <58529493+robertadriang@users.noreply.github.com>
2021-05-19 19:09:05 +0300

    Added createGroupTask

---
+++ src/main/java/Repository/GroupTaskRepository.java
@@ -0,0 +1,17 @@
+package Repository;
+
+
+import Entity.GroupTask;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class GroupTaskRepository {
+    public static void createGroupTask(GroupTask groupTask, EntityManagerFactory session){
+        EntityManager entityManager= session.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(groupTask);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
